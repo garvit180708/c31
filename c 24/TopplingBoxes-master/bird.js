07e45bbbdbf922462f072c5ac61eff0a7a5952fb
garvit180708 <80458951+garvit180708@users.noreply.github.com>
2021-07-17 18:44:12 +0530

    Add files via upload

---
+++ c 24/TopplingBoxes-master/bird.js
@@ -0,0 +1,12 @@
+class bird  extends BaseDesign{
+    constructor(x,y){
+super (x,y,50,50)
+this.image = loadImage("images/bird.png")
+    }
+  display() {
+    
+    //this.body.position.x = mouseX
+    //this.body.position.y = mouseY
+    super.display()
+  }
+}
